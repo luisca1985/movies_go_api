c4a30e005c866eec7d5246ff4f02f62d479ec307
Luis Camilo Jimenez Alvarez <luisca1985@gmail.com>
2022-01-12 23:56:45 -0500

    slices uses

--- tasks.go
+++ tasks.go
@@ -4,6 +4,18 @@ import (
 	"fmt"
 )
 
+type taskList struct {
+	tasks []*task
+}
+
+func (t *taskList) addToList(tl *task) {
+	t.tasks = append(t.tasks, tl)
+}
+
+func (t *taskList) removeFromList(index int) {
+	t.tasks = append(t.tasks[:index], t.tasks[index+1:]...)
+}
+
 type task struct {
 	name        string
 	description string
@@ -23,14 +35,27 @@ func (t *task) updateName(name string) {
 }
 
 func main() {
-	t := &task{
+	t1 := &task{
 		name:        "Complete Go course",
 		description: "Complete Go course this week",
 	}
-	fmt.Println(t)
-	fmt.Printf("%+v\n", t)
-	t.markAsCompleted()
-	t.updateName("End the Go course.")
-	t.updateDescription("End the course as soon as possible.")
-	fmt.Printf("%+v\n", t)
+	t2 := &task{
+		name:        "Complete Python course",
+		description: "Complete Python course this week",
+	}
+	t3 := &task{
+		name:        "Complete NodeJS course",
+		description: "Complete NodeJs course this week",
+	}
+
+	list := &taskList{
+		tasks: []*task{
+			t1, t2,
+		},
+	}
+	fmt.Println(list.tasks[0])
+	list.addToList(t3)
+	fmt.Println(len(list.tasks))
+	list.removeFromList(1)
+	fmt.Println(len(list.tasks))
 }
