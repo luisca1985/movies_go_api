2f77c123b099c67c08f64b22fea2ccb1aede107a
Luis Camilo Jimenez Alvarez <luisca1985@gmail.com>
2022-01-12 23:12:06 -0500

    use structs for the task list

---
+++ tasks.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+)
+
+type task struct {
+	name        string
+	description string
+	completed   bool
+}
+
+func (t task) markAsCompleted() {
+	t.completed = true
+}
+
+func (t task) updateDescription(description string) {
+	t.description = description
+}
+
+func (t task) updateName(name string) {
+	t.name = name
+}
+
+func main() {
+	t := task{
+		name:        "Complete Go course",
+		description: "Complete Go course this week",
+	}
+	fmt.Println(t)
+	fmt.Printf("%+v\n", t)
+	t.markAsCompleted()
+	t.updateName("End the Go course.")
+	t.updateDescription("End the course as soon as possible.")
+	fmt.Printf("%+v\n", t)
+}
