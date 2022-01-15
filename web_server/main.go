6ae1ef0520327287065234396fa4ab9537d95d09
Luis Camilo Jimenez Alvarez <luisca1985@gmail.com>
2022-01-14 21:59:36 -0500

    Add Logging Middleware

--- web_server/main.go
+++ web_server/main.go
@@ -3,6 +3,6 @@ package main
 func main() {
 	server := NewServer(":3000")
 	server.Handle("/", HandleRoot)
-	server.Handle("/api", server.AddMiddleware(HandleHome, CheckAuth()))
+	server.Handle("/api", server.AddMiddleware(HandleHome, CheckAuth(), Logging()))
 	server.Listen()
 }
--- web_server/middleware.go
+++ web_server/middleware.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"time"
 )
 
 func CheckAuth() Middleware {
@@ -18,4 +20,16 @@ func CheckAuth() Middleware {
 		}
 	}
 
+}
+
+func Logging() Middleware {
+	return func(f http.HandlerFunc) http.HandlerFunc {
+		return func(w http.ResponseWriter, r *http.Request) {
+			start := time.Now()
+			defer func() {
+				log.Println(r.URL.Path, time.Since(start))
+			}()
+			f(w, r)
+		}
+	}
 }
