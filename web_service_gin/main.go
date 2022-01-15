2b1ac7c7f7eda2ecbfcd7596edd762559b1fb645
Luis Camilo Jimenez Alvarez <luisca195@gmail.com>
2022-01-15 10:58:52 -0500

    declare the album struct

--- web_service_gin/main.go
+++ web_service_gin/main.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
-
 func main() {
-	fmt.Println("Hello world")
+	// album represents data about a record album.
+	type album struct {
+		ID     string  `json:"id"`
+		Title  string  `json:"title"`
+		Artist string  `json:"artist"`
+		Price  float64 `json:"price"`
+	}
 }
