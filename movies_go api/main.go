e0d7118881fbba9791a537427632494fa64a212b
Luis Camilo Jimenez Alvarez <luisca195@gmail.com>
2022-01-15 11:51:13 -0500

    create movie struct

--- movies_go api/main.go
+++ movies_go api/main.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
-
 func main() {
-	fmt.Println("Hello world")
+	// album represents data about a record album.
+	type movie struct {
+		ID           string   `json:"id"`
+		Title        string   `json:"title"`
+		ReleasedYear int      `json:"released_year"`
+		Rating       float64  `json:"rating"`
+		Genres       []string `json:"genres"`
+	}
 }
