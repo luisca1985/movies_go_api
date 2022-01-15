b27a82e891159c117bfa1cc1290c3dd6ede78cc4
Luis Camilo Jimenez Alvarez <luisca195@gmail.com>
2022-01-15 14:42:24 -0500

    include the router.POST function

--- web_service_gin/main.go
+++ web_service_gin/main.go
@@ -44,6 +44,7 @@ func postAlbums(c *gin.Context) {
 func main() {
 	router := gin.Default()
 	router.GET("/albums", getAlbums)
+	router.POST("/albums", postAlbums)
 	router.Run("localhost:8080")
 
 }
