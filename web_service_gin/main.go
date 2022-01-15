c8ef71dc0d94c47fe3f0e8d13028f24dbf08b0cb
Luis Camilo Jimenez Alvarez <luisca195@gmail.com>
2022-01-15 14:40:06 -0500

    add albums data to the list of albums

--- web_service_gin/main.go
+++ web_service_gin/main.go
@@ -26,6 +26,21 @@ func getAlbums(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, albums)
 }
 
+// postAlbums adds an album from JSON received in the request body.
+func postAlbums(c *gin.Context) {
+	var newAlbum album
+
+	// Call BindJSON to bind the received JSON to
+	// newAlbum.
+	if err := c.BindJSON(&newAlbum); err != nil {
+		return
+	}
+
+	// Add the new album to the slice.
+	albums = append(albums, newAlbum)
+	c.IndentedJSON(http.StatusCreated, newAlbum)
+}
+
 func main() {
 	router := gin.Default()
 	router.GET("/albums", getAlbums)
