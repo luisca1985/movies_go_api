8b298e4320c9b3708eda3cf5510efc16c75c4347
Luis Camilo Jimenez Alvarez <luisca195@gmail.com>
2022-01-16 11:04:40 -0500

    re-format find-movies.sql

--- movies_go_api/find-movies.sql
+++ movies_go_api/find-movies.sql
@@ -1,5 +1,13 @@
-SELECT movie.title, movie.released_year, movie.rating, GROUP_CONCAT(genre.genre)
-FROM movie 
-JOIN movie_genre ON movie.id = movie_genre.movie_id
-JOIN genre ON movie_genre.genre_id = genre.id
-GROUP BY movie.title, movie.released_year, movie.rating;
+SELECT
+  movie.*
+--   GROUP_CONCAT(genre.genre) AS genres
+FROM
+  movie
+  JOIN movie_genre ON movie.id = movie_genre.movie_id
+  JOIN genre ON movie_genre.genre_id = genre.id
+WHERE
+  genre.genre = 'Acción'
+-- GROUP BY
+--   movie.title,
+--   movie.released_year,
+--   movie.rating;
