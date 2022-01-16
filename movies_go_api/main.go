d50c3a60b114f395f35a81d41d85c1671b5bb60d
Luis Camilo Jimenez Alvarez <luisca195@gmail.com>
2022-01-16 11:08:22 -0500

    connect with db & find movies by genre func

--- movies_go_api/main.go
+++ movies_go_api/main.go
@@ -3,21 +3,88 @@ package main
 import (
 	"database/sql"
 	"fmt"
-	"github.com/gin-gonic/gin"
-	"github.com/go-sql-driver/mysql"
 	"log"
-	"net/http"
 	"os"
+
+	"github.com/go-sql-driver/mysql"
 )
 
 // movie represents data about a record album.
-type movie struct {
-	ID           string   `json:"id"`
+type Movie struct {
+	ID           int64    `json:"id"`
 	Title        string   `json:"title"`
 	ReleasedYear int      `json:"released_year"`
 	Rating       float64  `json:"rating"`
 	Genres       []string `json:"genres"`
 }
 
+var db *sql.DB
+
 func main() {
+	// Capture connection properties.
+	cfg := mysql.Config{
+		User:   os.Getenv("DBUSER"),
+		Passwd: os.Getenv("DBPASS"),
+		Net:    "tcp",
+		Addr:   "127.0.0.1:3306",
+		DBName: "movies_db",
+	}
+	// Get a database handle.
+	var err error
+	db, err = sql.Open("mysql", cfg.FormatDSN())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	pingErr := db.Ping()
+	if pingErr != nil {
+		log.Fatal(pingErr)
+	}
+	fmt.Println("Connected!")
+
+	//
+	movies, err := moviesByGenre("Drama")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Movies found: %v\n", movies)
+}
+
+// albumsByArtist queries for albums that have the specified artist name.
+func moviesByGenre(genre string) ([]Movie, error) {
+	// An albums slice to hold data from returned rows.
+	var movies []Movie
+
+	mov_rows, err := db.Query("SELECT movie.* FROM movie JOIN movie_genre ON movie.id = movie_genre.movie_id JOIN genre ON movie_genre.genre_id = genre.id WHERE genre.genre = ?", genre)
+	if err != nil {
+		return nil, fmt.Errorf("moviesByGenre %q: %v", genre, err)
+	}
+	defer mov_rows.Close()
+	// Loop through mov_rows, using Scan to assign column data to struct fields.
+	for mov_rows.Next() {
+		var mov Movie
+		if err := mov_rows.Scan(&mov.ID, &mov.Title, &mov.ReleasedYear, &mov.Rating); err != nil {
+			return nil, fmt.Errorf("moviesByGenre %q: %v", genre, err)
+		}
+
+		gen_rows, err := db.Query("SELECT genre.genre FROM movie_genre JOIN genre ON movie_genre.genre_id = genre.id WHERE movie_genre.movie_id = ?", mov.ID)
+		if err != nil {
+			return nil, fmt.Errorf("movie %q: %v", mov.ID, err)
+		}
+		defer gen_rows.Close()
+
+		for gen_rows.Next() {
+			var genr string
+			if err := gen_rows.Scan(&genr); err != nil {
+				return nil, fmt.Errorf("Genre %q: %v", genr, err)
+			}
+			mov.Genres = append(mov.Genres, genr)
+		}
+
+		movies = append(movies, mov)
+	}
+	if err := mov_rows.Err(); err != nil {
+		return nil, fmt.Errorf("moviesByGenre %q: %v", genre, err)
+	}
+	return movies, nil
 }
