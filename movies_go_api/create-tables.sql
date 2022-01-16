c991d39f4681e826f9549d0be4323fc3c959321e
Luis Camilo Jimenez Alvarez <luisca195@gmail.com>
2022-01-16 01:01:43 -0500

    query for all databases

--- movies_go_api/create-tables.sql
+++ movies_go_api/create-tables.sql
@@ -1,18 +1,56 @@
-
-  
-DROP TABLE IF EXISTS movie;
+DROP TABLE IF EXISTS movie,
+genre,
+movie_genre;
 CREATE TABLE movie (
-  id         INT AUTO_INCREMENT NOT NULL,
-  title      VARCHAR(128) NOT NULL,
-  artist     VARCHAR(255) NOT NULL,
-  price      DECIMAL(5,2) NOT NULL,
+  id INT AUTO_INCREMENT NOT NULL,
+  title VARCHAR(128) NOT NULL,
+  released_year INT NOT NULL,
+  rating DECIMAL(2, 1) NOT NULL,
   PRIMARY KEY (`id`)
 );
-
-INSERT INTO movie
-  (title, artist, price)
+CREATE TABLE genre (
+  id INT AUTO_INCREMENT NOT NULL,
+  genre VARCHAR(128) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+CREATE TABLE movie_genre (
+  id INT AUTO_INCREMENT NOT NULL,
+  movie_id INT NOT NULL,
+  genre_id INT NOT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT moviegenre_movie FOREIGN KEY (movie_id) REFERENCES movie (id) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT moviegenre_genre FOREIGN KEY (genre_id) REFERENCES genre (id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+ALTER TABLE
+  movie_genre
+ADD
+  (
+    INDEX moviegenre_movie_idx (movie_id ASC),
+    INDEX moviegenre_genre_idx (genre_id ASC)
+  );
+INSERT INTO
+  movie (title, released_year, rating)
 VALUES
-  ('Blue Train', 'John Coltrane', 56.99),
-  ('Giant Steps', 'John Coltrane', 63.99),
-  ('Jeru', 'Gerry Mulligan', 17.99),
-  ('Sarah Vaughan', 'Sarah Vaughan', 34.98);
+  ('The Godfather​', 1972, 5.0),
+  ('Kill Bill', 2004, 4.5),
+  ('The Terminator', 1984, 4.0),
+  ('The Hangover', 2009, 3.5);
+INSERT INTO
+  genre (genre)
+VALUES
+  ('Drama​'),
+  ('Crimen'),
+  ('Acción'),
+  ('Artes Marciales'),
+  ('Ciencia Ficción'),
+  ('Comedia');
+INSERT INTO
+  movie_genre (movie_id, genre_id)
+VALUES
+  (1, 1),
+  (1, 2),
+  (2, 3),
+  (2, 4),
+  (3, 3),
+  (3, 5),
+  (4, 6);
