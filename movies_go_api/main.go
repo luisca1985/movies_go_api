0e3eacdd4486bfa41d817e7c381e4e3a66eb6e79
Luis Camilo Jimenez Alvarez <luisca195@gmail.com>
2022-01-16 20:27:24 -0500

    all get endpoints are working

--- movies_go_api/main.go
+++ movies_go_api/main.go
@@ -5,13 +5,18 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+
+	"net/http"
 
 	"github.com/go-sql-driver/mysql"
+
+	"github.com/gin-gonic/gin"
 )
 
 // movie represents data about a record album.
 type Movie struct {
-	ID           int64    `json:"id"`
+	ID           int      `json:"id"`
 	Title        string   `json:"title"`
 	ReleasedYear int      `json:"released_year"`
 	Rating       float64  `json:"rating"`
@@ -42,22 +47,71 @@ func main() {
 	}
 	fmt.Println("Connected!")
 
-	//
-	movies, err := moviesByGenre("Drama")
+	router := gin.Default()
+	router.GET("/movies", getMovies)
+	router.GET("/movies/:id", getMovies)
+	router.Run("localhost:8080")
+}
+
+// getAlbumByID locates the album whose ID value matches the id
+// parameter sent by the client, then returns that album as a response.
+func getMovies(c *gin.Context) {
+	var movies []Movie
+	var movies_filtered []Movie
+	var err error
+
+	id, id_err := strconv.Atoi(c.Param("id"))
+	title := c.Query("title")
+	genre := c.Query("genre")
+	released_after, ra_err := strconv.Atoi(c.Query("released_after"))
+	released_before, rb_err := strconv.Atoi(c.Query("released_before"))
+	rating_higher_than, rh_err := strconv.ParseFloat(c.Query("rating_higher_than"), 64)
+	rating_lower_than, rl_err := strconv.ParseFloat(c.Query("rating_lower_than"), 64)
+
+	fmt.Println("Genre: ")
+	fmt.Println(genre)
+	if len(genre) > 0 {
+		movies, err = moviesByGenre(genre)
+	} else {
+		movies, err = allMovies()
+	}
+
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("Movies found: %v\n", movies)
+	// fmt.Printf("Movies found: %v\n", movies)
+
+	// c.IndentedJSON(http.StatusOK, movies)
+	// Loop over the list of albums, looking for
+	// an album whose ID value matches the parameter.
+	for _, mov := range movies {
+		if (mov.ID == id || id_err != nil) && (mov.Title == title || len(title) == 0) && (mov.ReleasedYear >= released_after || ra_err != nil) && (mov.ReleasedYear <= released_before || rb_err != nil) && (mov.Rating >= rating_higher_than || rh_err != nil) && (mov.Rating >= rating_lower_than || rl_err != nil) {
+			fmt.Printf("Movie: %v\n", mov)
+			movies_filtered = append(movies_filtered, mov)
+		}
+	}
+	if len(movies_filtered) > 0 {
+		c.IndentedJSON(http.StatusOK, movies_filtered)
+		return
+	}
+
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "movie not found"})
+}
+
+func allMovies() ([]Movie, error) {
+	movies, err := listMoviesByQuery("SELECT * FROM movie")
+	return movies, err
 }
 
 func moviesByGenre(genre string) ([]Movie, error) {
-	movies, err := listMoviesQuery("SELECT movie.* FROM movie JOIN movie_genre ON movie.id = movie_genre.movie_id JOIN genre ON movie_genre.genre_id = genre.id WHERE genre.genre = ?", genre)
+	movies, err := listMoviesByQuery("SELECT movie.* FROM movie JOIN movie_genre ON movie.id = movie_genre.movie_id JOIN genre ON movie_genre.genre_id = genre.id WHERE genre.genre = ?", genre)
 	return movies, err
 }
 
 // albumsByArtist queries for albums that have the specified artist name.
-func listMoviesQuery(query string, args ...interface{}) ([]Movie, error) {
+func listMoviesByQuery(query string, args ...interface{}) ([]Movie, error) {
 	// An albums slice to hold data from returned rows.
+
 	var movies []Movie
 
 	filterCriteria := "Criteria..."
