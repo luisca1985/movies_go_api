12b67ec021e8ac9bde5e8d6dde90b8c2c626dcd4
Luis Camilo Jimenez Alvarez <luisca195@gmail.com>
2022-01-20 01:43:00 -0500

    include long_title field in movie

--- sql_queries/create-tables.sql
+++ sql_queries/create-tables.sql
@@ -4,6 +4,7 @@ movie_genre;
 CREATE TABLE movie (
   id INT AUTO_INCREMENT NOT NULL,
   title VARCHAR(128) NOT NULL,
+  long_title VARCHAR(128) NOT NULL,
   released_year INT NOT NULL,
   rating DECIMAL(2, 1) NOT NULL,
   PRIMARY KEY (id),
@@ -31,29 +32,3 @@ ADD
     INDEX moviegenre_movie_idx (movie_id ASC),
     INDEX moviegenre_genre_idx (genre_id ASC)
   );
-INSERT INTO
-  movie (title, released_year, rating)
-VALUES
-  ('The Godfather​', 1972, 5.0),
-  ('Kill Bill', 2004, 4.5),
-  ('The Terminator', 1984, 4.0),
-  ('The Hangover', 2009, 3.5);
-INSERT INTO
-  genre (genre)
-VALUES
-  ('Drama​'),
-  ('Crimen'),
-  ('Acción'),
-  ('Artes Marciales'),
-  ('Ciencia Ficción'),
-  ('Comedia');
-INSERT INTO
-  movie_genre (movie_id, genre_id)
-VALUES
-  (1, 1),
-  (1, 2),
-  (2, 3),
-  (2, 4),
-  (3, 3),
-  (3, 5),
-  (4, 6);
